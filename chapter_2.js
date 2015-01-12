aba45b4a8bd7f53776ea584f07a8202c96e64dd7
vanessadean <vanessa.dean@gmail.com>
2015-01-11 19:42:04 -0500

    chess board

--- chapter_2.js
+++ chapter_2.js
@@ -1,13 +1,13 @@
 //looping a triangle
 var string = ""
-for (counter = 0; counter < 7; counter ++) {
+for (counter = 0; counter < 7; counter++) {
   string += "#";
   console.log(string);
 };
 
 //FizzBuzz
-for (counter = 1; counter <= 100; counter ++) {
-  var string = ""
+for (counter = 1; counter <= 100; counter++) {
+  var string = "";
   if (counter % 3 == 0) {
     string += "Fizz";
   };
@@ -20,3 +20,18 @@ for (counter = 1; counter <= 100; counter ++) {
     console.log(string);
   };
 };
+
+//Chess Board
+var boardSize = Number(prompt("Pick a board size", ""));
+var string = "";
+for (i = 0; i < boardSize; i++) {
+  for (j = 0; j < boardSize; j++) {
+    if (i % 2 == 0){
+      j % 2 == 0 ? string += " " : string += "#";
+    } else {
+      j % 2 != 0 ? string += " " : string += "#";
+    }
+  };
+  string += "\n";
+};
+console.log(string);
