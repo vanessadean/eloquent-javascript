eb037797c5caeb541326fc8942dcbfaa7004faad
vanessadean <vanessa.dean@gmail.com>
2015-01-11 19:29:51 -0500

    FizzBuzz

--- chapter_2.js
+++ chapter_2.js
@@ -1,7 +1,22 @@
 //looping a triangle
-
 var string = ""
 for (counter = 0; counter < 7; counter ++) {
   string += "#";
   console.log(string);
+};
+
+//FizzBuzz
+for (counter = 1; counter <= 100; counter ++) {
+  var string = ""
+  if (counter % 3 == 0) {
+    string += "Fizz";
+  };
+  if (counter % 5 == 0) {
+    string += "Buzz";
+  };
+  if (string == "") {
+    console.log(counter);
+  } else {
+    console.log(string);
+  };
 };
