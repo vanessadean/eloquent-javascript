7a0169e5f968b744f213593deb4b1e3556c3d2e7
vanessadean <vanessa.dean@gmail.com>
2015-01-13 14:40:51 -0500

    Bean Counting

--- chapter_3.js
+++ chapter_3.js
@@ -17,3 +17,22 @@ function isEven(N) {
     return isEven(N-2);
   }
 };
+
+//Bean Counting
+function countBs(string) {
+  var counter = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string.charAt(i) == "B")
+      counter++;
+  };
+  return counter;
+};
+
+function countChar(string, char) {
+  var counter = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string.charAt(i) == char)
+      counter++;
+  };
+  return counter;
+};
