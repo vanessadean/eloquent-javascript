00044a4d23016d74399c80c985a18ea909bb51a1
vanessadean <vanessa.dean@gmail.com>
2015-01-12 22:35:38 -0500

    min and recursion

---
+++ chapter_3.js
@@ -0,0 +1,19 @@
+//Minimum
+function min(a, b) {
+  return Math.min(a, b);
+};
+
+//Recursion
+function isEven(N) {
+  if (N == 0) {
+    return true;
+  }
+  else if (N == 1) {
+    return false;
+  } 
+  else if (N-2 < 0) {
+    return isEven(-N);
+  } else {
+    return isEven(N-2);
+  }
+};
