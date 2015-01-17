4d52c0f1615c1f30ba49087fddf705ec999c1cfc
vanessadean <vanessa.dean@gmail.com>
2015-01-17 15:14:07 -0500

    List

--- chapter_4.js
+++ chapter_4.js
@@ -52,3 +52,46 @@ function reverseArrayInPlace(array){
   }
   return array;
 };
+
+// A list
+function arrayToList(array) {
+  var list = null; 
+  for (i=array.length-1; i>=0; i--) {
+    list = prepend(array[i], list);
+  }
+  return list;
+};
+
+function prepend(value, list) {
+  list = {
+    value: value,
+    rest: list
+  };
+  return list;
+};
+
+function listToArray(list) {
+  var array = [];
+  for (var node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array;
+};
+
+function nth(list, index) {
+  var array = listToArray(list);
+  return array[index];
+};
+
+// function nth(list, n) {
+//   if (!list)
+//     return undefined;
+//   else if (n == 0)
+//     return list.value;
+//   else
+//     return nth(list.rest, n - 1);
+// }
+
+
+
+
