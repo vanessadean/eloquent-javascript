8a9edf5a7b9c17b5645b8ffb47a6badfc08f7611
vanessadean <vanessa.dean@gmail.com>
2015-01-19 16:10:20 -0500

    ch 5

--- chatper_5.js
+++ chatper_5.js
@@ -5,3 +5,47 @@ arrays.reduce(function(flat,current){
 },[]);
 
 // Mother-child age difference
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+function groupBy(array, groupOf) {
+  var groups = {};
+  array.forEach(function(element) {
+    var groupName = groupOf(element);
+    if (groupName in groups)
+      groups[groupName].push(element);
+    else
+      groups[groupName] = [element];
+  });
+  return groups;
+}
+
+var byCentury = groupBy(ancestry, function(person) {
+  return Math.ceil(person.died / 100);
+});
+
+for (var century in byCentury) {
+  var ages = byCentury[century].map(function(person) {
+    return person.died - person.born;
+  });
+  console.log(century + ": " + average(ages));
+}
+
+// Every and then some
+function every(array, test) {
+  for (var i = 0; i < array.length; i++) {
+    if (!test(array[i]))
+      return false;
+  }
+  return true;
+}
+
+function some(array, test) {
+  for (var i = 0; i < array.length; i++) {
+    if (test(array[i]))
+      return true;
+  }
+  return false;
+}
