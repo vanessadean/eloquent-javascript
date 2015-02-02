0641d9eaff2e624d1c3c0585f40fc3bb4ea7c0e9
vanessadean <vanessa.dean@gmail.com>
2015-02-02 16:00:50 -0500

    another cell

--- chapter_6.js
+++ chapter_6.js
@@ -22,3 +22,37 @@ Object.defineProperty(Vector.prototype, "length", {
 });
 
 //Another cell
+function TextCell(text) {
+  this.text = text.split("\n");
+}
+TextCell.prototype.minWidth = function() {
+  return this.text.reduce(function(width, line) {
+    return Math.max(width, line.length);
+  }, 0);
+};
+TextCell.prototype.minHeight = function() {
+  return this.text.length;
+};
+TextCell.prototype.draw = function(width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(line + repeat(" ", width - line.length));
+  }
+  return result;
+};
+
+function StretchCell(inner, width, height) {
+  this.inner = inner;
+  this.width = width;
+  this.height = height;
+};
+StretchCell.prototype.minWidth = function() {
+  return this.width + 2;
+};
+StretchCell.prototype.minHeight = function() {
+  return this.height;
+};
+StretchCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height);
+};
